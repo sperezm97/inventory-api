99c7d93edc005dab0210e01b75ef003350936563
Sebastian Perez <sperezm97@gmail.com>
2023-02-16 19:07:50 -0400

    feat: add db connection

---
+++ src/config/dataSource.ts
@@ -0,0 +1,17 @@
+import { DataSource } from 'typeorm';
+
+const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: false,
+  logging: false,
+  entities: [],
+  subscribers: [],
+  migrations: [],
+});
+
+export default AppDataSource;
