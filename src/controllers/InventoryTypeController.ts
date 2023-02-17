f078e4135d1fb74523d17850e7e076a99b1f3e3f
Sebastian Perez <sperezm97@gmail.com>
2023-02-17 15:24:16 -0400

    feat: create inventoryType controller

---
+++ src/controllers/InventoryTypeController.ts
@@ -0,0 +1,49 @@
+import {
+  Body, Delete, Get, Path, Post, Put, Route, Tags,
+} from 'tsoa';
+import { type InventoryType } from '../models';
+import {
+  getAllInventoryTypes,
+  getInventoryTypeById,
+  deleteInventoryTypeById,
+  putInventoryTypeById,
+  createInventoryType,
+} from '../repositories/InventoryTypeRepository';
+
+@Route('inventory-type')
+@Tags('InventoryType')
+export default class InventoryTypeController {
+  @Get('/')
+  static async getAllInventoryTypes(): Promise<InventoryType[]> {
+    return getAllInventoryTypes();
+  }
+
+  @Post('/')
+  static async createOneInventoryType(
+    @Body() body: Pick<InventoryType, 'description' | 'account' | 'status'>,
+  ): Promise<InventoryType | null> {
+    return createInventoryType(body);
+  }
+
+  @Get('{id}')
+  static async getOneInventoryType(
+    @Path() id: number,
+  ): Promise<InventoryType | null> {
+    return getInventoryTypeById(id);
+  }
+
+  @Put('{id}')
+  static async updateOneInventoryType(
+    @Path() id: number,
+      @Body() payload: Partial<InventoryType>,
+  ): Promise<InventoryType | null> {
+    return putInventoryTypeById(id, payload);
+  }
+
+  @Delete('{id}')
+  static async removeOneInventoryType(
+    @Path() id: number,
+  ): Promise<InventoryType | null> {
+    return deleteInventoryTypeById(id);
+  }
+}
